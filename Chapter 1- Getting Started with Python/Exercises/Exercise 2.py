d197ea122857a6eb8d2df5828069a5ce5be40026
jaydeeci <144207824+jaydeeci@users.noreply.github.com>
2023-10-03 11:41:03 +0400

    Exercise 2.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise 2.py
@@ -0,0 +1,10 @@
+import sys
+
+# Get the Python version
+python_version = sys.version
+
+# Print the Python version
+print("Python Version:")
+print(python_version)
+
+#This code shows us the python version that is currently being used by using the print command.
