5d618c78858a0b1a7d1ed9e044ef12bc6f299788
jaydeeci <144207824+jaydeeci@users.noreply.github.com>
2023-10-03 11:47:23 +0400

    Create Exercise 5.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise 5.py
@@ -0,0 +1,10 @@
+import math
+
+# Input: Get the radius from the user
+radius = float(input("Enter the radius of the circle: "))
+
+# Calculate the area of the circle
+area = math.pi * (radius ** 2)
+
+# Display the result
+print(f"The area of the circle with radius {radius} is {area:.2f}")
