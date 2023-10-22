fef70ea755e6fc5d291b90397ff8e4cabb12d7ba
jaydeeci <144207824+jaydeeci@users.noreply.github.com>
2023-10-22 16:12:09 +0400

    Create Exercise 4.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 4.py
@@ -0,0 +1,7 @@
+# List of people to invite to dinner
+guests = ["Albert Einstein", "Elvis Preseley", "Elon Musk"]
+
+# Print dinner invitation messages to each person
+for guest in guests:
+    invitation = f"Dear {guest}, we are inviting you over to our vacation house for dinner, this has been such a long awaited moment for us. We are looking forward to meeting you :). "
+    print(invitation)
