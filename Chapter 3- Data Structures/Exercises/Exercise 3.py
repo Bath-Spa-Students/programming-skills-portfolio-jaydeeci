6d166ad48ce42a8f5295ed95683ae8458377ed6d
jaydeeci <144207824+jaydeeci@users.noreply.github.com>
2023-10-22 16:07:11 +0400

    Create Exercise 3.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 3.py
@@ -0,0 +1,6 @@
+# Create a list of car examples
+favorite_cars = ["Jeep Wrangler 392", "Jeep Tj", "Nissan VTC"]
+
+# Print statements about these cars
+for car in favorite_cars:
+    print(f"I would like to own a {car} as a project car to build with my dad.")
