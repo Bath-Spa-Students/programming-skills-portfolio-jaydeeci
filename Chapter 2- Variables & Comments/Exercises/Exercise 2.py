dac1f8501da1c153e9b78d03adbdba0cc0f36a59
jaydeeci <144207824+jaydeeci@users.noreply.github.com>
2023-10-22 15:44:42 +0400

    Exercise 2.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise 2.py
@@ -0,0 +1,6 @@
+# Quote from Albert Einstein
+quote = "I am thankful for all those who said no to me. It's because of them I'm doing it myself."
+author = "Albert Einstein"
+
+# Print the quote and its author
+print(f'{author} once said, "{quote}"')
