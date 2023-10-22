dcab827d3c341fafe44b62e03d89f7c87274ffd2
jaydeeci <144207824+jaydeeci@users.noreply.github.com>
2023-10-22 15:51:26 +0400

    Create Exercise 3.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise 3.py
@@ -0,0 +1,20 @@
+# Define a name with leading and trailing whitespace
+name = "\t \n   Ifrah Imtiaz \t \n"
+
+# Print the name with the surrounding whitespace
+print("Name with Whitespace:")
+print(name)
+
+# Use the lstrip() function to remove leading whitespace
+print("\nUsing lstrip():")
+print(name.lstrip())
+
+# Use the rstrip() function to remove trailing whitespace
+print("\nUsing rstrip():")
+print(name.rstrip())
+
+# Use the strip() function to remove both leading and trailing whitespace
+print("\nUsing strip():")
+print(name.strip())
+
+
