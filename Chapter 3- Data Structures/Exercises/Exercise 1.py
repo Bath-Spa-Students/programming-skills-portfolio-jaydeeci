5869d841b88887cc1916b9e9a6c2253db814e7cb
jaydeeci <144207824+jaydeeci@users.noreply.github.com>
2023-10-22 16:00:54 +0400

    Create Exercise 1.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 1.py
@@ -0,0 +1,6 @@
+# List of friend's names
+names = ["Reign", "Ysa", "Cacai", "Francis", "Fiona", "Chrystianne", "Dana"]
+
+# Print each person's name one at a time
+for name in names:
+    print(name)
