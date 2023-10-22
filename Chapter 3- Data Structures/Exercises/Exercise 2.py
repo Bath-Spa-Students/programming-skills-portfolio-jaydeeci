132b9416e4d09f3c57010e5b4ee73a5227e6ceee
jaydeeci <144207824+jaydeeci@users.noreply.github.com>
2023-10-22 16:03:37 +0400

    Create Exercise 2.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 2.py
@@ -0,0 +1,7 @@
+# List of friend names
+names = ["Reign", "Ysa", "Cacai", "Francis", "Fiona", "Chrystianne", "Dana"]
+
+# Print a personalized message for each person
+for name in names:
+    message = f"Hi, {name}! You're annoying."
+    print(message)
