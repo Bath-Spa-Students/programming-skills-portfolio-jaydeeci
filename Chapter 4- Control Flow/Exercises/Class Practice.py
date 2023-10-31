567dbe51c770d310ae5d6a0da9f62dacfcb40a9e
jaydeeci <144207824+jaydeeci@users.noreply.github.com>
2023-10-31 10:17:09 +0400

    Create Class Practice.py

---
+++ Chapter 4- Control Flow/Exercises/Class Practice.py
@@ -0,0 +1,37 @@
+'''Temperature = float(input("Enter Temperature : "))
+result = 0
+if Temperature > 38.123 :
+    result = "Fever"
+else:
+    result = "Normal"
+print("Your Temperature :  "+str(result))'''
+
+'''age = int(input("Enter your age :"))
+driving_experience = float (input("How long have you had a license in the past? "))
+
+if age > 18:
+    if driving_experience >2:
+        print("Elegible for License")
+    else :
+      print("Inelegible for License")
+else :
+         print("Inelegible for license")'''
+
+percentage = float(input ("Enter your percentage "))
+if percentage >= 90:
+    print ("Your grade is A ")
+elif percentage >= 80:
+    print ("Your grade is B ")
+elif percentage >= 70:
+    print ("Your grade is C ")
+elif percentage >= 60:
+    print ("Your grade is D ")
+elif percentage >= 40:
+    print ("Your grade is E ")
+else :
+    print ("Your grade is F ")
+
+
+
+
+
