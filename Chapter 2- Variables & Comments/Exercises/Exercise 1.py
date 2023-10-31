3ba9f557e8e93753c51917fc7a8c485d7f6eeff6
jaydeeci <144207824+jaydeeci@users.noreply.github.com>
2023-10-31 10:15:25 +0400

    Create Exercise 1.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise 1.py
@@ -0,0 +1,11 @@
+# Assign a message to a variable
+message = "Hello, World!"
+
+# Print the initial message
+print("Initial Message:", message)
+
+# Change the value of the variable to a new message
+message = "Goodbye, World."
+
+# Print the new message
+print("New Message:", message)
