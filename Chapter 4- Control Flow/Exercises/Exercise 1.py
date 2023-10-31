081badb0bc20ad750e32b7002261c710c700eef1
jaydeeci <144207824+jaydeeci@users.noreply.github.com>
2023-10-31 10:17:14 +0400

    Create Exercise 1.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise 1.py
@@ -0,0 +1,9 @@
+alien_color = 'green'  # Change to 'green', 'yellow', or 'red' to test different versions
+
+if alien_color == 'green':
+    print("Congratulations! You just earned 5 points.")
+
+alien_color = 'red'  # Change to 'green', 'yellow', or 'red' to test different versions
+
+if alien_color == 'green':
+    print("Congratulations! You just earned 5 points.")
