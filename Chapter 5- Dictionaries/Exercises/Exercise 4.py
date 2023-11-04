87cf2ffed11d878c6ed096e30595ffe2fc730860
jaydeeci <144207824+jaydeeci@users.noreply.github.com>
2023-11-04 12:27:38 +0400

    Update Exercise 4.py

--- Chapter 5- Dictionaries/Exercises/Exercise 4.py
+++ Chapter 5- Dictionaries/Exercises/Exercise 4.py
@@ -1,1 +1,16 @@
+rivers = {
+    'Pasig': 'Philippines',
+    'River Thames': 'United Kingdom',
+    'Orange River': 'South Africa',
+    }
 
+for river, country in rivers.items():
+    print("The " + river.title() + " flows through " + country.title() + ".")
+
+print("\nThe following rivers are included in this data set:")
+for river in rivers.keys():
+    print("- " + river.title())
+
+print("\nThe following countries are included in this data set:")
+for country in rivers.values():
+    print("- " + country.title())
