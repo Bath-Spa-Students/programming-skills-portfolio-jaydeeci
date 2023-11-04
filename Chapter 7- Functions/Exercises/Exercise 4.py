ed4dd9c2afa5a4b32598db4b4970e9358c6b35dc
jaydeeci <144207824+jaydeeci@users.noreply.github.com>
2023-11-04 13:07:44 +0400

    Update Exercise 4.py

--- Chapter 7- Functions/Exercises/Exercise 4.py
+++ Chapter 7- Functions/Exercises/Exercise 4.py
@@ -1,1 +1,8 @@
+def make_shirt(size='large', message='I live for the cars'):
+    """Summarize the shirt that's going to be made."""
+    print("\nI'm going to make a " + size + " t-shirt.")
+    print('It will say, "' + message + '"')
 
+make_shirt()
+make_shirt(size='small')
+make_shirt('extra small', 'Cars are overrated')
