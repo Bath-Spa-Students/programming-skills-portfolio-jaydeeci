c1a9e1d26aff2d4a899618e25489fa3d9b43265c
jaydeeci <144207824+jaydeeci@users.noreply.github.com>
2023-11-04 11:25:27 +0400

    Update Exercise 3.py

--- Chapter 4- Control Flow/Exercises/Exercise 3.py
+++ Chapter 4- Control Flow/Exercises/Exercise 3.py
@@ -1,1 +1,8 @@
-j
+alien_color = 'brown'
+
+if alien_color == 'white':
+  print("You just earned 1 point")
+elif alien_color == 'black':
+  print("You just earned 5 points")
+else:
+  print("You just earned 10 points")
