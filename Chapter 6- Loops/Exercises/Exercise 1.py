89f1de1d2ef4da323f6d0877b1af1080fcd03bce
jaydeeci <144207824+jaydeeci@users.noreply.github.com>
2023-11-04 12:35:34 +0400

    Update Exercise 1.py

--- Chapter 6- Loops/Exercises/Exercise 1.py
+++ Chapter 6- Loops/Exercises/Exercise 1.py
@@ -1,1 +1,9 @@
+prompt = "\nWhat type of pizza do you want?"
+prompt += "\nEnter 'done' when you are done: "
 
+while True:
+    pizza = input(prompt)
+    if topping != 'done':
+        print("  I'll add " + pizza + " to your order.")
+    else:
+        break
