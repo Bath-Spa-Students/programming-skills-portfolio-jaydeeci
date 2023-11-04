bb317d81db811946d3c708e7761ae35e42b423f9
jaydeeci <144207824+jaydeeci@users.noreply.github.com>
2023-11-04 12:15:03 +0400

    Update Exercise 2.py

--- Chapter 5- Dictionaries/Exercises/Exercise 2.py
+++ Chapter 5- Dictionaries/Exercises/Exercise 2.py
@@ -1,1 +1,22 @@
+glossary = {
+    'variable': 'A value that changes depending on the conditions or information processed in the program.',
+    'data': 'Any information translated in a way that it is efficient.',
+    'list': 'A collection of items in a particular order.',
+    'loop': 'Work through a collection of items, one at a time.',
+    'tuple': "A set of unchangeable values that are separatec by commas and they store multiple data types..",
+    }
 
+word = 'variable'
+print("\n" + word.title() + ": " + glossary[word])
+
+word = 'data'
+print("\n" + word.title() + ": " + glossary[word])
+
+word = 'list'
+print("\n" + word.title() + ": " + glossary[word])
+
+word = 'loop'
+print("\n" + word.title() + ": " + glossary[word])
+
+word = 'tuple'
+print("\n" + word.title() + ": " + glossary[word])
