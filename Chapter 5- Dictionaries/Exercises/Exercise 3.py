b19112a1213a1fc6b80b93ee3f624003d35fde86
jaydeeci <144207824+jaydeeci@users.noreply.github.com>
2023-11-04 12:23:07 +0400

    Update Exercise 3.py

--- Chapter 5- Dictionaries/Exercises/Exercise 3.py
+++ Chapter 5- Dictionaries/Exercises/Exercise 3.py
@@ -3,13 +3,11 @@ glossary = {
     'data': 'Any information translated in a way that it is efficient.',
     'list': 'A collection of items in a particular order.',
     'loop': 'Work through a collection of items, one at a time.',
-    'dictionary': "A collection of key-value pairs.",
-    'key': 'The first item in a key-value pair in a dictionary.',
+    'tuple': "A set of unchangeable values that are separatec by commas and they store multiple data types..",
+    'boolean expression': 'An expression that evaluates to True or False.',
     'value': 'An item associated with a key in a dictionary.',
     'conditional test': 'A comparison between two values.',
-    'float': 'A numerical value with a decimal component.',
-    'boolean expression': 'An expression that evaluates to True or False.',
-    }
+}
 
 for word, definition in glossary.items():
     print("\n" + word.title() + ": " + definition)
