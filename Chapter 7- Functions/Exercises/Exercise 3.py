36780e5f5fd432b95bfdaa047b56d69327f9aeca
jaydeeci <144207824+jaydeeci@users.noreply.github.com>
2023-11-04 13:06:03 +0400

    Update Exercise 3.py

--- Chapter 7- Functions/Exercises/Exercise 3.py
+++ Chapter 7- Functions/Exercises/Exercise 3.py
@@ -1,1 +1,7 @@
+def make_shirt(size, message):
+    """Summarize the shirt that's going to be made."""
+    print("\nI'm going to make a " + size + " t-shirt.")
+    print('It will say, "' + message + '"')
 
+make_shirt('medium', 'I live for the cars')
+make_shirt(message="Readability counts.", size='small')
