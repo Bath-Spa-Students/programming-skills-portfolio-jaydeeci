8e5d50453bf8a5ee9ec90a4e7752d8fa2b25eef8
jaydeeci <144207824+jaydeeci@users.noreply.github.com>
2023-11-04 13:22:45 +0400

    Update Exercise 3.py

--- Chapter 5- Dictionaries/Exercises/Exercise 3.py
+++ Chapter 5- Dictionaries/Exercises/Exercise 3.py
@@ -7,6 +7,8 @@ glossary = {
     'boolean expression': 'An expression that evaluates to True or False.',
     'value': 'An item associated with a key in a dictionary.',
     'conditional test': 'A comparison between two values.',
+    'function': 'Reusable code to perform a single and related action.',
+    'control flow': 'The order where artys of a computer program - instructions, statements and function calls are executed and evaluated.
 }
 
 for word, definition in glossary.items():
