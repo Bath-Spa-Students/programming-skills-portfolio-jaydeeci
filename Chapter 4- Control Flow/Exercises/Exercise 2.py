eda5d0e3b757e099cb96cb40f120a41c50bfd4b8
jaydeeci <144207824+jaydeeci@users.noreply.github.com>
2023-11-04 11:17:04 +0400

    Update Exercise 2.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise 2.py
@@ -0,0 +1,13 @@
+alien_color = 'white'  # Change to 'white' or another color to test different versions
+
+if alien_color == 'white':
+    print("Congratulations! You just earned 1 points for shooting the green alien.")
+else:
+    print("Congratulations! You just earned 5 points for shooting the alien.")
+
+alien_color = 'black'  # Change to 'white' or another color to test different versions
+
+if alien_color == 'white':
+    print("Congratulations! You just earned 1 points for shooting the green alien.")
+else:
+    print("Congratulations! You just earned 5 points for shooting the alien.")
