3446b16a5e2bd9e8154d21d13589055b7e7a152f
jaydeeci <144207824+jaydeeci@users.noreply.github.com>
2023-11-04 13:09:20 +0400

    Update Exercise 5.py

--- Chapter 7- Functions/Exercises/Exercise 5.py
+++ Chapter 7- Functions/Exercises/Exercise 5.py
@@ -1,1 +1,8 @@
+def describe_city(city, country='philippines'):
+    """Describe a city."""
+    msg = city.title() + " is in " + country.title() + "."
+    print(msg)
 
+describe_city('batangas')
+describe_city('reykjavik', 'iceland')
+describe_city('illocos')
