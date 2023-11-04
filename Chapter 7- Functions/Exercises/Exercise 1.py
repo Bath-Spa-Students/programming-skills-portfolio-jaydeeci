4e1c2d9cb213e64ba87c088a1ffb044239ff0f85
jaydeeci <144207824+jaydeeci@users.noreply.github.com>
2023-11-04 13:01:24 +0400

    Update Exercise 1.py

--- Chapter 7- Functions/Exercises/Exercise 1.py
+++ Chapter 7- Functions/Exercises/Exercise 1.py
@@ -1,1 +1,6 @@
+def display_message():
+    """Display a message about what I am learning in this chapter."""
+    msg = "I'm learning to store code in functions."
+    print(msg)
 
+display_message()
