b464abfb27fd948ede2b67f6719aff5bc14e9624
jaydeeci <144207824+jaydeeci@users.noreply.github.com>
2023-11-04 11:44:17 +0400

    Update Exercise 1.py

--- Chapter 5- Dictionaries/Exercises/Exercise 1.py
+++ Chapter 5- Dictionaries/Exercises/Exercise 1.py
@@ -1,1 +1,11 @@
+person = {
+    'first_name': 'Ysa',
+    'last_name': 'Villadelgado',
+    'age': 19,
+    'city': 'Manila',
+    }
 
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
